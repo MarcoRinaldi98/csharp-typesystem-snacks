ac34b5856a4ab4cb7b7d301deb6cd7b6519b16b1
Marco Rinaldi <marcorinaldi98@libero.it>
2023-09-06 14:22:37 +0200

    snack 5 completato

---
+++ snack5/Program.cs
@@ -0,0 +1,11 @@
+﻿// SNACK 5
+Console.WriteLine("Inserisci un numero:");
+int numero = int.Parse(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine(numero);
+} else
+{
+    Console.WriteLine(numero + 1);
+}
