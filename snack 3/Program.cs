920d067c22ed1b14353e0b2beb3832908e3c94fd
Marco Rinaldi <marcorinaldi98@libero.it>
2023-09-06 14:09:45 +0200

    snack 3 completato

---
+++ snack 3/Program.cs
@@ -0,0 +1,9 @@
+﻿// SNACK 3
+int somma = 0;
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Inserisci un numero:");
+    somma += int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine($"La somma di tutti i numeri inseriti è: {somma}");
