2bf0dab6bc346a2173dcb7403d315634176f2680
Marco Rinaldi <marcorinaldi98@libero.it>
2023-09-06 13:54:42 +0200

    snack 1 completato

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// SNACK 1
+Console.WriteLine("Inserisci il primo numero:");
+int numero1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Inserisci il secondo numero:");
+int numero2 = int.Parse(Console.ReadLine());
+
+if (numero1 > numero2)
+{
+    Console.WriteLine($"Il numero piu grande inserito è {numero1}");
+}
+else if (numero1 < numero2)
+{
+    Console.WriteLine($"Il numero piu grande inserito è {numero2}");
+} else
+{
+    Console.WriteLine("I due numeri soino uguali!");
+}
