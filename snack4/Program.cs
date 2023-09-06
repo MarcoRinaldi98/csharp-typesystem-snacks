2bef595b7878a52430efad0d74a99ed9ed1e4fbd
Marco Rinaldi <marcorinaldi98@libero.it>
2023-09-06 14:18:37 +0200

    snack 4 completato

---
+++ snack4/Program.cs
@@ -0,0 +1,17 @@
+﻿// SNACK 4
+
+int[] numeri = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int somma = 0;
+
+for (int i = 0; i < numeri.Length; i++)
+{
+    somma += numeri[i];
+}
+
+float media = somma / numeri.Length;
+
+
+Console.WriteLine($"La somma dei numeri da 2 a 10 è {somma}");
+Console.WriteLine($"La media dei numeri da 2 a 10 è {media}");
+
+
